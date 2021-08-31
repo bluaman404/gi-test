766262496a87f98e6239b1144b179d22e6ea0a30
blueman404 <1144571314@qq.com>
2021-08-31 20:01:03 +0800

    hello

---
+++ src/main/java/com/example/GitTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+/**
+ * @Description:
+ * @Author: blueman404
+ * @Date: 2021/8/31  19:59
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
